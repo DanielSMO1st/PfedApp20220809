c0accfdbb611e470ea9828c22ca4b47ab7957ee8
DanielSMO1st <daniel.pfennigsdorf@siemens.com>
2022-09-13 15:39:09 +0200

    gradle.xml-and-vcs.xml-pfed20220901-commit-message - extracted string to ressouce

--- app/src/main/java/com/example/pfedapp20220809/FirstFragment.kt
+++ app/src/main/java/com/example/pfedapp20220809/FirstFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import com.example.pfedapp20220809.databinding.FragmentFirstBinding
 
